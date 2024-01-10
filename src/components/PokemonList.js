89d89fd94fea935063e1152480e148ccefebf68d
Edona Lleshi <edona.lleshi95@gmail.com>
2024-01-10 16:00:22 +0100

    next-prew-button

--- src/components/PokemonList.js
+++ src/components/PokemonList.js
@@ -2,12 +2,13 @@ import { useState , useEffect } from "react";
 
 export default function PokemonList() {
   const [pokemon, setPokemon] = useState([]);
+  const [offset, setOffset]= useState(0);
 
   useEffect (() => {
   
     async function loadPokemon() {
     try {
-      const response = await fetch("https://pokeapi.co/api/v2/pokemon");
+      const response = await fetch(`https://pokeapi.co/api/v2/pokemon?offset=${offset}`);
       const data = await response.json();
       setPokemon(data.results);
     } catch (error) {
@@ -16,16 +17,26 @@ export default function PokemonList() {
   }
 
   loadPokemon ();
-}, []);
+}, [offset]);
 
-  return (
-    <main>
-    
-      <ul>
-        {pokemon.map(({ name }) => (
-          <li key={name}>{name}</li>
-        ))}
-      </ul>
-    </main>
-  );
+const handleNextPage = () => {
+  setOffset((prevOffset) => prevOffset + 20);
+};
+const handlePreviousPage = () => {
+  if (offset >=20){
+setOffset((prevOffset) => prevOffset - 20);
+  }
+};
+
+return (
+  <main>
+    <button type="button" onClick={handlePreviousPage}>Previous Page</button>
+    <button type="button" onClick={handleNextPage}>Next Page</button>
+    <ul>
+      {pokemon.map(({ name }) => (
+        <li key={name}>{name}</li>
+      ))}
+    </ul>
+  </main>
+);
 }
